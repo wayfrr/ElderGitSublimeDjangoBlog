df8da250448729409b816513ac2ff587752160bd
radha <network4nairs@gmail.com>
2021-08-05 09:08:03 +0530

    Initial Commit

---
+++ theblog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ('title', 'title_tag','author','body')
+
+		widgets = {
+			'title':forms.TextInput(attrs={'class':'form-control'}),
+			'title_tag':forms.TextInput(attrs={'class':'form-control'}),
+			'author':forms.Select(attrs={'class':'form-select'}),
+			'body':forms.Textarea(attrs={'class':'form-control'}),
+
+		}
---
+++ theblog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+
+#def home(request):
+#	return render(request,'home.html', {})
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+	model = Post
+	template_name = 'article_details.html'
+
+
+class AddPostView(CreateView):
+	model = Post
+	form_class = PostForm
+	template_name = 'add_post.html'
+	#fields = '__all__'
+	#fields = ('title','body')
+		
