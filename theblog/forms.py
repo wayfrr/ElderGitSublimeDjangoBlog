75f6b2fb1de4736b6d9b9edc4af3a8d79788090c
radha <network4nairs@gmail.com>
2021-08-05 22:59:26 +0530

    Completed 10th video-code in it

--- theblog/forms.py
+++ theblog/forms.py
@@ -13,3 +13,17 @@ class PostForm(forms.ModelForm):
 			'body':forms.Textarea(attrs={'class':'form-control'}),
 
 		}
+
+
+class EditForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ('title', 'title_tag','body')
+
+		widgets = {
+			'title':forms.TextInput(attrs={'class':'form-control'}),
+			'title_tag':forms.TextInput(attrs={'class':'form-control'}),
+			#'author':forms.Select(attrs={'class':'form-select'}),
+			'body':forms.Textarea(attrs={'class':'form-control'}),
+
+		}
--- theblog/views.py
+++ theblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm,EditForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -11,6 +12,8 @@ from .forms import PostForm
 class HomeView(ListView):
 	model = Post
 	template_name = 'home.html'
+	ordering = [ '-post_date' ]
+	#ordering = [ '-id' ]
 
 
 class ArticleDetailView(DetailView):
@@ -25,3 +28,15 @@ class AddPostView(CreateView):
 	#fields = '__all__'
 	#fields = ('title','body')
 		
+
+class UpdatePostView(UpdateView):
+	model = Post
+	form_class = EditForm
+	template_name = 'update_post.html'
+	#fields = ['title', 'title_tag', 'body']
+
+
+class DeletePostView(DeleteView):
+	model = Post
+	template_name = 'delete_post.html'
+	success_url = reverse_lazy('home')
