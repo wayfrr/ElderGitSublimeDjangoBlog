c13bf61598a9cf1976de1c548a61b61778ff1b30
radha <network4nairs@gmail.com>
2021-08-09 00:08:45 +0530

    Upto and including Video No.28

--- ablog/settings.py
+++ ablog/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+#added by me 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+#JohnElder Settings BASE_DIR = os.path.dirname(os.path.dirname(os.path.....))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,6 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- theblog/models.py
+++ theblog/models.py
@@ -19,8 +19,25 @@ class Category(models.Model):
 		#return reverse('article-detail',args=(str(self.id)) )
 		return reverse('home')
 
+class Profile(models.Model):
+	user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+	bio = models.TextField()
+	profile_pic = models.ImageField(null=True, blank=True,upload_to="images/profile/")
+	website_url = models.CharField(max_length=255, null=True, blank=True)
+	facebook_url = models.CharField(max_length=255, null=True, blank=True)
+	twitter_url = models.CharField(max_length=255, null=True, blank=True)
+	instagram_url = models.CharField(max_length=255, null=True, blank=True)
+	pinterest_url = models.CharField(max_length=255, null=True, blank=True)
+
+	def __str__(self):
+		return str(self.user)
+
+
+
+
 class Post(models.Model):
 	title = models.CharField(max_length=255)
+	header_image = models.ImageField(null=True, blank=True,upload_to="images/")
 	title_tag = models.CharField(max_length=255)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	body = RichTextField(blank=True, null=True)
